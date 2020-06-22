5e202839803b29048cc9bac60f959eb01f7a9bad
idmacleod <idmacleod@gmail.com>
2020-06-22 14:19:16 +0100

    Added Developer and DatabaseAdmin classes, all tested

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+    private DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before() {
+        databaseAdmin = new DatabaseAdmin("Jim", "KG 80 45 88B", 40000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jim", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNi() {
+        assertEquals("KG 80 45 88B", databaseAdmin.getNi());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(40000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(10000.00);
+        assertEquals(50000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(400.00 ,databaseAdmin.payBonus(), 0.01);
+    }
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+    private Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Martin", "TY 87 67 55B", 50000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Martin", developer.getName());
+    }
+
+    @Test
+    public void hasNi() {
+        assertEquals("TY 87 67 55B", developer.getNi());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(50000.00, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        developer.raiseSalary(10000.00);
+        assertEquals(60000.00, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals(500.00 ,developer.payBonus(), 0.01);
+    }
 }
+
